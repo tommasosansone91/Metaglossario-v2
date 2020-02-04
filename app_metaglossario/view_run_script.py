00dbe1aab3c2cba8fc42216d1d639a84c3ca97b3
tommasosansone91 <tommaso.sansone@mail.polimi.it>
2020-02-04 12:29:08 +0100

    attivato anche pour simple model in run_scritp

--- app_metaglossario/view_run_script.py
+++ app_metaglossario/view_run_script.py
@@ -21,7 +21,7 @@ def run_script(request):
     # erase_glossary_entry()
     erase_acquired_terminology()
 
-    # pour_entire_simple_model()
+    pour_entire_simple_model()
     pour_entire_file_model()
     # pour_latest_file()
     
